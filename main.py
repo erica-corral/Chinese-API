37a9b9a006eedf044bb443627c4c80e72b6e6aef
erica-corral <101758819+erica-corral@users.noreply.github.com>
2022-04-17 15:18:01 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from fastapi import FastAPI
-from LAC import LAC
+import uvicorn
+import jieba
 from cihai.core import Cihai
 
 #globals for processes
 app = FastAPI()
-lac = LAC(mode="seg")
 c = Cihai()
 if not c.unihan.is_bootstrapped:  # download and install Unihan db for Cihai if not installed already
-    c.unihan.bootstrap(unihan_options)
+    c.unihan.bootstrap({})
 
 
-@app.get(/segment/{input})
-def segment():
-    seg_result = lac.run(input)
-    return seg_result
+@app.get("/segment/{input}")
+def segment(input):
+    print(input)
+    seg_result = jieba.cut(input,cut_all=False)
+    return " ".join(seg_result)
 
-@app.get(/define/{input})
-def define():
-    query = c.unihan.lookup_char(input)
+@app.get("/define/{input}")
+def define(input):
+    query = c.unihan.lookup_char(input).first()
     return query
