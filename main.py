26c9fbc8b38e2152608a8d05618ba2d2258711a9
erica-corral <101758819+erica-corral@users.noreply.github.com>
2022-04-17 19:44:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import jieba
-from cihai.core import Cihai
+from chinese_english_lookup import Dictionary
+from pypinyin import pinyin
 
 #globals for processes
 app = FastAPI()
-c = Cihai()
-if not c.unihan.is_bootstrapped:  # download and install Unihan db for Cihai if not installed already
-    c.unihan.bootstrap({})
+d = Dictionary()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/segment/{input}")
@@ -18,5 +28,16 @@ def segment(input):
 
 @app.get("/define/{input}")
 def define(input):
-    query = c.unihan.lookup_char(input).first()
-    return query
+    word_entry = d.lookup(input)
+    kDefinition = word_entry.definition_entries[0].definitions
+    kHanyuPinyin = processpinyin(input)
+    kTraditionalVariant = word_entry.trad
+    return {"kDefinition": kDefinition,"kHanyuPinyin": kHanyuPinyin, "kTraditionalVariant":kTraditionalVariant }
+
+def processpinyin(input):
+    initial_pinyin = pinyin(input)
+    final_pinyin = ""
+    for word in initial_pinyin:
+        final_pinyin = final_pinyin+word[0]
+    return final_pinyin
+
